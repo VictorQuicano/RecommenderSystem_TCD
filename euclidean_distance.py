c781778d56b2f516dfde259a29c652e3dc8b720f
Leon Davis <ldavis@unsa.edu.pe>
2025-06-27 13:23:43 -0500

    Add euclidean_distance

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -1,13 +1,14 @@
-import pandas as pd
 import math
+from typing import List
 
-def euclidean_distance(df: pd.DataFrame, user1: str, user2: str) -> float:
-    values1 = df[user1].tolist()
-    values2 = df[user2].tolist()
+#Params
+#values1 = df["Heather"].tolist()
+#values2 = df["Thomas"].tolist()
 
+def euclidean_distance(values1: List[float], values2: List[float]) -> float:
     squared_sum = 0
     for v1, v2 in zip(values1, values2):
-        if pd.notna(v1) and pd.notna(v2):
+        if v1 is not None and v2 is not None:
             diff = v1 - v2
             squared_sum += diff * diff
 
